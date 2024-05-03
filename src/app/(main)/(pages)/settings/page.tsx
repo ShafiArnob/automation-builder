6cb6b7056464cef7e54b144cf64a833e3ca341c6
your name <shafiarnob@gmail.com>
2024-05-03 06:37:39 +0600

    setup upload care

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,10 +1,25 @@
 import ProfileForm from "@/components/forms/profile-form";
 import React from "react";
+import ProfilePicture from "./_components/profile-picture";
+import { db } from "@/lib/db";
 
 type Props = {};
 
 const Settings = (props: Props) => {
-  //WIP: Wire up profile picture
+  // remove Image
+  const removeProfileImage = async () => {
+    "use server";
+    const response = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        profileImage: "",
+      },
+    });
+    return response;
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
@@ -17,6 +32,11 @@ const Settings = (props: Props) => {
             Add or update your information
           </p>
         </div>
+        <ProfilePicture
+          onDelete={removeProfileImage}
+          // userImage={user?.profileImage || ""}
+          // onUpload={uploadProfileImage}
+        />
         <ProfileForm />
         {/* <ProfilePicture
           onDelete={removeProfileImage}
