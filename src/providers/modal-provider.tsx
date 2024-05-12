e65352ea53df9918849fe1da1d5bd80522b0ee36
your name <shafiarnob@gmail.com>
2024-05-12 08:25:05 +0600

    modal provider

---
+++ src/providers/modal-provider.tsx
@@ -0,0 +1,73 @@
+"use client";
+import { createContext, useContext, useEffect, useState } from "react";
+
+interface ModalProviderProps {
+  children: React.ReactNode;
+}
+
+export type ModalData = {};
+
+type ModalContextType = {
+  data: ModalData;
+  isOpen: boolean;
+  setOpen: (modal: React.ReactNode, fetchData?: () => Promise<any>) => void;
+  setClose: () => void;
+};
+
+export const ModalContext = createContext<ModalContextType>({
+  data: {},
+  isOpen: false,
+  setOpen: (modal: React.ReactNode, fetchData?: () => Promise<any>) => {},
+  setClose: () => {},
+});
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [data, setData] = useState<ModalData>({});
+  const [showingModal, setShowingModal] = useState<React.ReactNode>(null); //it is for what modal am i showing. it is for any type of modal for any type of data
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  const setOpen = async (
+    modal: React.ReactNode,
+    fetchData?: () => Promise<any>
+  ) => {
+    if (modal) {
+      //the modal will fetch the data for you if you want
+      if (fetchData) {
+        setData({ ...data, ...(await fetchData()) } || {});
+      }
+      // set the modal you want to render
+      setShowingModal(modal);
+      setIsOpen(true);
+    }
+  };
+
+  // when you are closing the modal
+  const setClose = () => {
+    setIsOpen(false);
+    setData({});
+  };
+
+  if (!isMounted) return null;
+
+  return (
+    <ModalContext.Provider value={{ data, setOpen, setClose, isOpen }}>
+      {children}
+      {showingModal}
+    </ModalContext.Provider>
+  );
+};
+
+export const useModal = () => {
+  const context = useContext(ModalContext);
+  if (!context) {
+    throw new Error("useModal must be used within the modal provider");
+  }
+  return context;
+};
+
+export default ModalProvider;
