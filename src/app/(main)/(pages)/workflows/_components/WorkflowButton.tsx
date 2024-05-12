18d52a9efefcdfb440c9a07288459030f66782cf
your name <shafiarnob@gmail.com>
2024-05-12 08:39:48 +0600

    custom bottom drawer modal for global usage

--- src/app/(main)/(pages)/workflows/_components/WorkflowButton.tsx
+++ src/app/(main)/(pages)/workflows/_components/WorkflowButton.tsx
@@ -1,12 +1,21 @@
 "use client";
+import CustomModal from "@/components/global/CustomModal";
 import { Button } from "@/components/ui/button";
+import { useModal } from "@/providers/modal-provider";
 import { Plus } from "lucide-react";
 import React from "react";
 
 type Props = {};
 
 const WorkflowButton = (props: Props) => {
+  const { setOpen, setClose } = useModal();
   const handleClick = () => {
+    <CustomModal
+      title="Create a Workflow Automation"
+      subheading="Workflows are powerful taht helpyou automate tasks"
+    >
+      <h1>Hello</h1>
+    </CustomModal>;
     console.log("Workflow Button");
   };
   return (
